1beaf74006c73a2052ef0d3354c109a3ce95083d
Jay <dice205test@gmail.com>
2020-04-09 11:20:59 +0800

    Initial commit for nightwatch

---
+++ nightwatch.conf.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "src_folders": ["tests"],
+
+    "webdriver": {
+        "start_process": true,
+        "server_path": "node_modules/.bin/chromedriver.exe",
+        "port": 9515
+    },
+
+    "test_settings": {
+        "default": {
+            "desiredCapabilities": {
+                "browserName": "chrome",
+                "javascriptEnabled": true,
+                //"chromeOptions" : {
+                //   "args" : ["--headless"]
+                //}
+            }
+        }
+    },
+}
